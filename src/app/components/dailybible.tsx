d45265d00fcdd13498501d8a02ab5178592fba9b
DevSilveira <devFilipeSilveira@hotmail.com>
2025-04-04 04:25:28 -0300

    Adição de API, Daily verse 0.3 {alpha}
    Adicionada API para demonstração díaria de versos biblicos.
    
    Estilização no futuro.

---
+++ src/app/components/dailybible.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function DailyVerse() {
+  const [verse, setVerse] = useState("");
+
+  useEffect(() => {
+    const fetchVerse = async () => {
+      try {
+        const response = await fetch("https://bible-api.com/john%203:16");
+        const data = await response.json();
+        setVerse(`${data.text} (${data.reference})`);
+      } catch (error) {
+        setVerse("Não foi possível carregar o versículo hoje.");
+      }
+    };
+
+    fetchVerse();
+  }, []);
+
+  
+  return ( 
+  
+  <div className="container-bible">
+    <h2>Verse of the day</h2>
+  <p>{verse}</p>
+  </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import TodoList from "./components/Todolist";
-
+import DailyBible from "./components/dailybible";
 export default function Home() {
   return (
+
+    
 <main>
   <TodoList />
+  <DailyBible />
 </main>
 
   
